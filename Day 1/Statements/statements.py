d2d813d6f8dd830a99382a51c009079f3f69c16e
jhashank <jhashank@nuevesolutions.in>
2026-05-20 16:21:49 +0000

    Add initial Python statements and syntax examples

---
+++ Day 1/Statements/statements.py
@@ -0,0 +1,18 @@
+# Statements are instructions that a Python interpreter can execute. 
+# A statement is a logical instruction that the Python interpreter can execute. 
+# A statement can be a single line of code or a block of code. 
+# A statement can be an expression, a function call, a loop, a conditional statement, etc.
+
+print("Hello, World!") # This is a statement
+x = 5 # This is a statement
+y = 10 # This is a statement
+if x < y: # This is a statement
+    print("x is less than y") # This is a statement
+else: # This is a statement
+    print("x is greater than or equal to y") # This is a statement  
+
+# Semicolons can be used to separate multiple statements on the same line.
+x = 5; y = 10; print(x + y) # This is a statement with multiple statements on the same line.
+
+""" However, it is not recommended to use semicolons to separate statements on the same line, 
+as it can make the code less readable. It is better to write each statement on a separate line."""
---
+++ Day 1/Syntax/Syntax.py
@@ -0,0 +1,29 @@
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line. 
+# Python uses indentation to indicate a block of code.
+if 5 > 2:
+    print("Five is greater than two!")
+else:
+    print("Five is not greater than two!")
+
+# Indentation errors
+
+# if 5 > 2:
+# print("Five is greater than two!")
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+#     print("Five is greater than two!")
+#        print("Five is greater than two!")
+
+# Python Comments
+# Comments can be used to explain code, and to make the code more readable. 
+# Comments can be used to prevent execution when testing code.
+
+# This is a comment
+
+# This is a comment, and this will not be executed
+
+# multi line comment
+"""This is a comment
+This is also a comment"""
