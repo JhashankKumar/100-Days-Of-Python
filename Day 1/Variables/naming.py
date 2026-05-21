8f5aa6306d2cc0986a97a54102816c2ee3d87dca
jhashank <jhashank@nuevesolutions.in>
2026-05-21 01:01:15 +0000

    Add variable naming rules and examples for valid and invalid variable names

---
+++ Day 1/Variables/naming.py
@@ -0,0 +1,18 @@
+# Naming Rules
+# 1. A variable name must start with a letter or the underscore character
+# 2. A variable name cannot start with a number
+# 3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+# 5. A variable name cannot be any of the Python keywords.
+
+# Valid variable names
+my_variable = 10
+myVariable = 10
+_my_variable = 10
+my_variable_2 = 10 
+
+# Invalid variable names
+# 2my_variable = 10 # variable name cannot start with a number
+# my-variable = 10 # variable name cannot contain a hyphen
+# my variable = 10 # variable name cannot contain a space
+# my.variable = 10 # variable name cannot contain a dot
---
+++ Day 1/Variables/variables.py
@@ -0,0 +1,37 @@
+# variable creation
+my_variable = 10
+print(my_variable)
+
+# variable assignment
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# variable reassignment
+x = 5
+print(x)
+x = 10
+print(x)
+
+# reassigning a variable with a different data type
+""" In Python, you can reassign a variable to a different data type. 
+This is one of the features of Python that makes it a dynamically typed language."""
+x = 5
+print(x)
+x = "Hello, World!"
+print(x)
+
+# casting a variable to a different data type
+x = 5
+print(x)
+x = str(x)
+print(x)
+
+y = "10"
+print(y)
+y = int(y)
+print(y)
+y = float(y)
+print(y)
+
