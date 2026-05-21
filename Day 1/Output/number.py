2c205473d959f4d500ebe737e8e4492493e6a06d
jhashank <jhashank@nuevesolutions.in>
2026-05-21 01:00:26 +0000

    Add number printing and basic arithmetic operations example

---
+++ Day 1/Output/number.py
@@ -0,0 +1,21 @@
+# Print Numbers
+print(5)
+print(10)
+print(15)
+
+# Print the result of a mathematical expression
+print(5 + 10)
+print(10 - 5)
+print(5 * 10)
+print(10 / 5)
+
+# print a number with a string
+print("The result of 5 + 10 is " + str(5 + 10))
+print("The result of 10 - 5 is " + str(10 - 5))
+print("The result of 5 * 10 is " + str(5 * 10))
+print("The result of 10 / 5 is " + str(10 / 5))
+
+# print a number with a string using f-string
+print(f"The result of 5 + 10 is {5 + 10}")
+print(f"The result of 10 - 5 is {10 - 5}")
+print(f"The result of 5 * 10 is {5 * 10}")
