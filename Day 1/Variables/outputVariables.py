c40d89a4b8534aba24d8f8a76a8972db253064ab
jhashank <jhashank@nuevesolutions.in>
2026-05-22 16:15:54 +0000

    Add examples of variable assignments and basic operations in Python

---
+++ Day 1/Variables/outputVariables.py
@@ -0,0 +1,20 @@
+x = 10
+print(x)  # Output: 10 
+y = "Hello, World!"
+print(y)  # Output: Hello, World!
+z = 3.14
+print(z)  # Output: 3.14
+
+print(x, y, z)  # Output: 10 Hello, World! 3.14
+
+x = "Hello"
+y = "World"
+a = 42
+b = 38
+print(x, y)  # Output: Hello World
+print(x + " " + y)  # Output: Hello World
+print(a + b)  # Output: 80
+print(a, b)  # Output: 42 38
+print(a+y)  # Output: 42Hello, World! (concatenation of integer and string)
+
+
