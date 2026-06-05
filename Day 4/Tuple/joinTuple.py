f7d470f069d1af36992dd0f590c10ac0b506ca11
jhashank <jhashank@nuevesolutions.in>
2026-06-05 01:44:17 +0000

    Add joinTuple.py to demonstrate tuple joining techniques in Python using + and * operators.

---
+++ Day 4/Tuple/joinTuple.py
@@ -0,0 +1,15 @@
+# join tuple using the + operator
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+joined_tuple = tuple1 + tuple2  
+print(joined_tuple)  # Output: (1, 2, 3, 4, 5, 6)
+
+# join tuple using the * operator
+tuple1 = (1, 2, 3)
+joined_tuple = tuple1 * 2
+print(joined_tuple)  # Output: (1, 2, 3, 1, 2, 3)
+
+triple_tuple = (1, 2, 3)
+joined_tuple = triple_tuple * 3
+print(joined_tuple)  # Output: (1, 2, 3, 1, 2, 3, 1, 2, 3)  
+
