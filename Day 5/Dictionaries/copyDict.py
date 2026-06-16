c08bf4a6c99d32c98bcd2a0bdc91eaf88168ad9a
jhashank <jhashank@nuevesolutions.in>
2026-06-16 15:58:55 +0000

    Add copyDict.py to demonstrate copying a dictionary and explain the difference between copy() and dict() methods

---
+++ Day 5/Dictionaries/copyDict.py
@@ -0,0 +1,30 @@
+"""
+Copy a Dictionary
+You cannot copy a dictionary simply by typing dict2 = dict1, 
+because: dict2 will only be a reference to dict1, 
+and changes made in dict1 will automatically also be made in dict2
+"""
+
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "country": "USA",
+    "email": "john@yopmail.com",
+    "phone": "123-456-7890"
+}
+
+my_dict_copy = my_dict.copy()
+print(my_dict_copy)
+
+my_dict_copy2 = dict(my_dict)
+print(my_dict_copy2)
+
+"""
+what is the difference between copy() and dict() methods?
+The copy() method creates a shallow copy of the dictionary, 
+meaning that it copies the dictionary structure and its keys and values, 
+but if the values are mutable objects (like lists or other dictionaries), 
+changes made to those mutable objects in the original dictionary will also be reflected in the copied dictionary.
+The dict() method creates a new dictionary from an existing dictionary or iterable of key-value pairs.  
+"""
