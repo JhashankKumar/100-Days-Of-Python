49781bd01b4b13b881a58215a75c65f8f8656a84
jhashank <jhashank@nuevesolutions.in>
2026-06-19 01:53:21 +0000

    Added tricky.py for understanding the lexicographic comparision conditional rendering

---
+++ Day 6/practice/tricky.py
@@ -0,0 +1,54 @@
+a = "33"
+b = "200"
+"""
+for a = 33 and b = 200, the output will be:
+b is greater than a
+
+for a = "200" and b = "33", the output will be:
+none of the above
+"""
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+elif a < b:
+    print("a is less than b")
+else:
+    print("none of the above")
+
+# string comparison
+a = "200"
+b = "33"
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+elif a < b:     
+    print("a is less than b")
+else:    
+    print("none of the above")
+
+
+# number comparison
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:    
+    print("a and b are equal")
+elif a < b:     
+    print("a is less than b")
+else:    
+    print("none of the above")
+
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:    
+    print("a and b are equal")
+elif a < b:     
+    print("a is less than b")
+else:    
+    print("none of the above")
+    
